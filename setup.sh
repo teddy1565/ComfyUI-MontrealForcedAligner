04a9b9b41f109780b2f4e056e7805e33d1524a87
Teddy Xiong <teddy1565@gmail.com>
2026-04-02 11:25:27 +0800

    ci: 🎡 update setup shell

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 conda install -c conda-forge montreal-forced-aligner
 mfa model download acoustic mandarin_mfa
-mfa model download dictionary mandarin_mfa
+mfa model download dictionary mandarin_china_mfa
 
 sudo apt install git g++ make automake autoconf sox libtool subversion \
                  python2.7 python3 zlib1g-dev gfortran wget unzip \
