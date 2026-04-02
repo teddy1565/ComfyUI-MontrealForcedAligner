01fc44cc59fb84ad61e0804459b9e855b40c7e36
Teddy Xiong <teddy1565@gmail.com>
2026-04-02 10:46:34 +0800

    ci: 🎡 update setup shell

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ mfa model download dictionary mandarin_mfa
 sudo apt install git g++ make automake autoconf sox libtool subversion \
                  python2.7 python3 zlib1g-dev gfortran wget unzip \
                  libatlas-base-dev
+sudo apt-get install autoconf automake cmake curl g++ git graphviz \
+    libatlas3-base libtool make pkg-config subversion unzip wget zlib1g-dev
 sudo apt install libfst-tools libfst-dev
